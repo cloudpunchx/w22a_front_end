33aa097457bfdf9db4d4f4a22762fbfba18864f2
cloudpunchx <siobhanbonardi@hotmail.com>
2023-02-13 22:39:44 -0700

    Forgot to do first commit. Created api calls and forms to enter data. Tested and working as expected.

---
+++ src/views/HomePage.vue
@@ -0,0 +1,173 @@
+<template>
+    <div>
+        <h1>Cloudpunch Candy</h1>
+        <h4>We have many candy options! Add your own to the list or edit an existing candy.</h4>
+
+        <!-- Div for GET candy -->
+        <!-- why won't candy ID display? -->
+        <div v-for="(candy,i) in candy"
+        :key="i"
+        >
+            <p>Id: {{ candy.id }} | {{ candy.candyName }} | {{ candy.variety }}</p>
+        </div>
+
+        <!-- Div for POST candy -->
+        <div>
+            <v-container>
+                <h3>Add to Candy:</h3>
+                <v-col>
+                    <v-text-field
+                    v-model="candyName"
+                    label="Candy Name"
+                    >
+                    </v-text-field>
+                </v-col>
+                <v-col>
+                    <v-text-field
+                    v-model="category"
+                    label="Category"
+                    >
+                    </v-text-field>
+                    <button @click="addCandy">Submit</button>
+                    <div v-if="message">
+                        <p>{{ message }}</p>
+                    </div>
+                </v-col>
+            </v-container>
+        </div>
+
+        <!-- Div for PATCH candy -->
+        <div>
+            <v-container>
+                <h3>Edit Existing Candy:</h3>
+                <v-col>
+                    <v-text-field
+                    v-model="candyId"
+                    label="Candy Id"
+                    >
+                    </v-text-field>
+                    <v-text-field
+                    v-model="candyName"
+                    label="Candy Name"
+                    >
+                    </v-text-field>
+                    <button @click="editCandy">Submit</button>
+                    <div v-if="message">
+                        <p>{{ message }}</p>
+                    </div>
+                </v-col>
+            </v-container>
+        </div>
+
+        <!-- Div for PATCH candy -->
+        <div>
+            <v-container>
+                <h3>Delete Candy:</h3>
+                <v-col>
+                    <v-text-field
+                    v-model="candyName"
+                    label="Candy Name"
+                    >
+                    </v-text-field>
+                    <button @click="deleteCandy">Submit</button>
+                    <div v-if="message">
+                        <p>{{ message }}</p>
+                    </div>
+                </v-col>
+            </v-container>
+        </div>
+
+    </div>
+</template>
+
+<script>
+import axios from "axios";
+
+    export default {
+        name: "HomePage",
+        data() {
+            return {
+                candy: [],
+                candyName: "",
+                candyId: "",
+                category: "",
+                message: ""
+            }
+        },
+        methods: {
+            getCandy() {
+                axios.request({
+                    url: `${process.env.VUE_APP_BASE_DOMAIN}/api/candy`,
+                    method: "GET",
+                }).then((response)=>{
+                    this.candy = response.data;
+                }).catch((error)=>{
+                    error = "Something went wrong, try again."
+                    alert(error);
+                })
+            },
+            addCandy() {
+                axios.request({
+                    url: `${process.env.VUE_APP_BASE_DOMAIN}/api/candy`,
+                    method: "POST",
+                    data: {
+                        candyName: this.candyName,
+                        category: this.category
+                    }
+                }).then(()=>{
+                    this.clearTextBox();
+                    this.message = "Successfully added candy!";
+                    
+                }).catch((error)=>{
+                    this.clearTextBox();
+                    this.message = error;
+                })
+            },
+            editCandy() {
+                axios.request({
+                    url: `${process.env.VUE_APP_BASE_DOMAIN}/api/candy`,
+                    method: "PATCH",
+                    data: {
+                        candyId: this.candyId,
+                        candyName: this.candyName,
+                    }
+                }).then(()=>{
+                    this.clearTextBox();
+                    this.message = "Successfully edited candy!";
+                    
+                }).catch((error)=>{
+                    this.clearTextBox();
+                    this.message = error;
+                })
+            },
+            deleteCandy() {
+                axios.request({
+                    url: `${process.env.VUE_APP_BASE_DOMAIN}/api/candy`,
+                    method: "DELETE",
+                    data: {
+                        candyName: this.candyName,
+                    }
+                }).then(()=>{
+                    this.clearTextBox();
+                    this.message = "Successfully deleted candy!";
+                    
+                }).catch((error)=>{
+                    this.clearTextBox();
+                    this.message = error;
+                })
+            },
+            clearTextBox(){
+                this.candyName = "";
+                this.category = "";
+                this.candyId = "";
+            }
+        },
+        mounted () {
+            this.getCandy();
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
